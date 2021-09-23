cb24df57abc8ce8f5072b36afbcfda3c9c94af76
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-22 22:40:01 -0300

    Input options implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import Header from './Header';
 import './App.css';
+import Feed from "./Feed";
+import Header from './Header';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header/>
 
       {/** App Body */}
       <div className="app__body">
-        <Sidebar />
+          <Sidebar />
+          <Feed />
+          {/** Widgets */}
       </div>
-        {/** Sidebar */}
-        {/** Feed */}
-        {/** Widgets */}
-    
     </div>
-  )     
+  );     
 }
 
 export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,16 +3,25 @@ import { Avatar } from "@material-ui/core";
 import "./Sidebar.css";
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+
+        </div>
+    )
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img src="https://images.unsplash.com/photo-1558470598-a5dda9640f68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" alt="" />
         <Avatar className="sidebar__avatar" />
         <h2>Paulo Thomaz</h2>
         <h4>p.thomaz.junior@gmail.com</h4>
       </div>
 
-      <div className="sidebar__status">
+      <div className="sidebar__stats">
         <div className="sidebar__stat">
           <p>Who viewd you</p>
           <p className="sidebar__statNumber">2,543</p>
@@ -25,6 +34,11 @@ function Sidebar() {
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programing')}
+        {recentItem('softwareengineering')}
+        {recentItem('design')}
+        {recentItem('developer')}
       </div>
     </div>
   );
