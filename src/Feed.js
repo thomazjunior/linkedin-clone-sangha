8af1327a511b5b480e96c648a98c856009092598
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-23 12:00:29 -0300

    Initializing implementation of firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import CreatIcon from "@material-ui/icons/Create";
 import ImageIcon from "@material-ui/icons/Image";
@@ -6,9 +6,27 @@ import InputOption from "./InputOption";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
-
+import Post from "./Post";
+import { db } from "firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -16,23 +34,32 @@ function Feed() {
           <CreatIcon />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className="feed__inputOptions">
-            <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
-            <InputOption Icon={SubscriptionsIcon} title="Video"
-            color="#E7A33E" />
-            <InputOption Icon={EventNoteIcon} title="Event"
-            color="#C0CBCD" />
-            <InputOption 
-                Icon={CalendarViewDayIcon}
-                title="Write article"
-                color="#7FC15E"
-            />
-
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
+      {/**Posts */}
+      {posts.map((post) => (
+        <Post />
+      ))}
+
+      <Post
+        name="Paulo Thomaz"
+        description="This is a test"
+        message="WOW this worked"
+      />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC3C-GYTMMs2ifVUVjMVo-dFbLWRDHR6yU",
+    authDomain: "linkedin-clone-sangha.firebaseapp.com",
+    projectId: "linkedin-clone-sangha",
+    storageBucket: "linkedin-clone-sangha.appspot.com",
+    messagingSenderId: "193775985779",
+    appId: "1:193775985779:web:fc50422570f902a81c7606",
+    measurementId: "G-3GL231ZHME"
+  };y
