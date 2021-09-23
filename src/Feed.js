3c2b4c3380a382edaf6597a5042b25967ef81bec
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-23 15:15:32 -0300

    Fetch with Firebase implemented and initializing implementation of redux

--- src/Feed.js
+++ src/Feed.js
@@ -7,13 +7,15 @@ import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import Post from "./Post";
-import { db } from "firebase";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function Feed() {
+  const [input, setInput] = useState("");
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) =>
+    db.collection("posts").orderBy('timestamp', 'desc').onSnapshot((snapshot) =>
       setPosts(
         snapshot.docs.map((doc) => ({
           id: doc.id,
@@ -25,6 +27,16 @@ function Feed() {
 
   const sendPost = (e) => {
     e.preventDefault();
+
+    db.collection("posts").add({
+      name: "Sonny Sangha",
+      description: "this is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
   };
 
   return (
@@ -33,7 +45,7 @@ function Feed() {
         <div className="feed__input">
           <CreatIcon />
           <form>
-            <input type="text" />
+            <input value={input} onChange={e => setInput (e.target.value)} type="text" />
             <button onClick={sendPost} type="submit">
               Send
             </button>
@@ -51,15 +63,17 @@ function Feed() {
         </div>
       </div>
       {/**Posts */}
-      {posts.map((post) => (
-        <Post />
+      {posts.map(({id, data: {name, description, message, photoUrl } }) => (
+        <Post 
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
       ))}
 
-      <Post
-        name="Paulo Thomaz"
-        description="This is a test"
-        message="WOW this worked"
-      />
+     
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase';
+import 'firebase/firestore';
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyC3C-GYTMMs2ifVUVjMVo-dFbLWRDHR6yU",
@@ -8,4 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "193775985779",
     appId: "1:193775985779:web:fc50422570f902a81c7606",
     measurementId: "G-3GL231ZHME"
-  };y
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
