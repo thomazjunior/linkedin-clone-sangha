f6ae661c52d2ecdf6952cb6d0e47b7043bdca9ae
Paulo Thomaz Junior <p.thomaz.junior@gmail.com>
2021-09-23 16:56:48 -0300

    Redux implemented and initializing implementation of login view

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import Feed from "./Feed";
-import Header from './Header';
-import Sidebar from './Sidebar';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Header/>
+      <Header />
 
-      {/** App Body */}
-      <div className="app__body">
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
           <Sidebar />
           <Feed />
           {/** Widgets */}
-      </div>
+        </div>
+      )}
     </div>
-  );     
+  );
 }
 
 export default App;
